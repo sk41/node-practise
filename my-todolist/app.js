9ec34f5314ab3279d803e7eb9949e9ba1253a943
sk41 <41099873+sk41@users.noreply.github.com>
2019-03-26 10:45:08 -0500

    modification of todo list

--- my-todolist/app.js
+++ my-todolist/app.js
@@ -27,6 +27,7 @@ we create an empty one in the form of an array before continuing */
 /* Adding an item to the to do list */
 .post('/todo/add/', urlencodedParser, function(req, res) {
     if (req.body.newtodo != '') {
+
         req.session.todolist.push(req.body.newtodo);
     }
     res.redirect('/todo');
@@ -35,6 +36,7 @@ we create an empty one in the form of an array before continuing */
 /* Deletes an item from the to do list */
 .get('/todo/delete/:id', function(req, res) {
     if (req.params.id != '') {
+        console.log(req.params.id);
         req.session.todolist.splice(req.params.id, 1);
     }
     res.redirect('/todo');
